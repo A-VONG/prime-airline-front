6788f54ca9fe3017256942b3fd882083ca64148b
Allane <Allane@Allane-MaxBook-Pro.local>
2023-09-21 16:25:28 +0200

    add status design

--- pages/status.vue
+++ pages/status.vue
@@ -1,20 +1,85 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <p :class="this.api1 ? 'greenApi' : 'redApi'">
-        Data access service is : {{ this.api1 ? "Up" : "Down" }}
-      </p>
-      <p :class="this.api2 ? 'greenApi' : 'redApi'">
-        Flight service is : {{ this.api2 ? "Up" : "Down" }}
-      </p>
-      <p :class="this.api3 ? 'greenApi' : 'redApi'">
-        Booking service is : {{ this.api3 ? "Up" : "Down" }}
-      </p>
-      <p :class="this.api4 ? 'greenApi' : 'redApi'">
-        FrontApi service is : {{ this.api4 ? "Up" : "Down" }}
-      </p>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row justify="space-around">
+      <v-card width="400">
+        <v-img
+          height="200px"
+          src="https://www.racksolutions.com/news/app/uploads/AdobeStock_90603827-1-scaled.jpeg"
+        >
+          <v-app-bar flat color="rgba(0, 0, 0, 0)">
+            <v-toolbar-title class="text-h6 white--text pl-0">
+              Status des services
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-app-bar>
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">
+            {{ new Date().toLocaleDateString("fr") }}
+          </div>
+          <v-timeline align-top dense>
+            <v-timeline-item small :color="this.api1 ? 'green' : 'red'">
+              <div>
+                <div class="font-weight-normal">
+                  <strong>Le service d'accés aux données</strong>
+                </div>
+                <div>
+                  {{
+                    this.api1
+                      ? "Pas de problème detecté"
+                      : "Nous rencontrons un problème technique. Veuillez reéssayer utérieurement"
+                  }}
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item small :color="this.api2 ? 'green' : 'red'">
+              <div>
+                <div class="font-weight-normal">
+                  <strong>Le service d'accés aux vols</strong>
+                </div>
+                <div>
+                  {{
+                    this.api2
+                      ? "Pas de problème detecté"
+                      : "Nous rencontrons un problème technique. Veuillez reéssayer utérieurement"
+                  }}
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item small :color="this.api3 ? 'green' : 'red'">
+              <div>
+                <div class="font-weight-normal">
+                  <strong>Le service d'accés aux reséversions</strong>
+                </div>
+                <div>
+                  {{
+                    this.api3
+                      ? "Pas de problème detecté"
+                      : "Nous rencontrons un problème technique. Veuillez reéssayer utérieurement"
+                  }}
+                </div>
+              </div>
+            </v-timeline-item>
+            <v-timeline-item small :color="this.api4 ? 'green' : 'red'">
+              <div>
+                <div class="font-weight-normal">
+                  <strong>Le service d'accés aux api fronts</strong>
+                </div>
+                <div>
+                  {{
+                    this.api4
+                      ? "Pas de problème detecté"
+                      : "Nous rencontrons un problème technique. Veuillez reéssayer utérieurement"
+                  }}
+                </div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
